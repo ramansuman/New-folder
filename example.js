3b6f9d34f2e0ced552fc1d8da788008f63807df1
raman <ramansuman98.rs@gmail.com>
2024-01-01 21:50:03 +0530

    remove web.html

--- example.js
+++
@@ -1,9 +0,0 @@
-var a = [];
-a.unshift(5);
-a.unshift(22);
-a.shift();
-a.unshift(3, [4, 5]);
-a.shift();
-a.shift();
-a.shift();
-console.log(a);
--- test2.js
+++
@@ -1,9 +0,0 @@
-const a = {};
-const b = { key: 'b' };
-const c = { key: 'b' };
-
-a[b] = 123;
-a[c] = 456;
-
-console.log(a[b]);
-console.log(b==c)
